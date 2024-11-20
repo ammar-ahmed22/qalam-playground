846245f2bd10470c2c59cd4530f901688e69c3fa
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-11-19 19:32:06 -0500

    fix: gitignore server target

--- server/target/debug/deps/server-42e3355eda8d5faa.d
+++
@@ -1,5 +0,0 @@
-/Users/ammar/Documents/Projects/qalam-playground/server/target/debug/deps/libserver-42e3355eda8d5faa.rmeta: src/main.rs
-
-/Users/ammar/Documents/Projects/qalam-playground/server/target/debug/deps/server-42e3355eda8d5faa.d: src/main.rs
-
-src/main.rs:
--- server/target/debug/deps/server-87066bf1af07eec2.d
+++
@@ -1,5 +0,0 @@
-/Users/ammar/Documents/Projects/qalam-playground/server/target/debug/deps/libserver-87066bf1af07eec2.rmeta: src/main.rs
-
-/Users/ammar/Documents/Projects/qalam-playground/server/target/debug/deps/server-87066bf1af07eec2.d: src/main.rs
-
-src/main.rs:
