89f16f621aa22103427f37fa69f989ea14117156
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-11-21 00:51:40 -0500

    fix: formatting

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,15 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::serde::{Serialize, Deserialize};
 use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
 use rocket::Request;
+use rocket_cors::{AllowedOrigins, Cors, CorsOptions};
 use std::process::Command;
-use rocket_cors::{ AllowedOrigins, Cors, CorsOptions };
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Code {
-    raw: String
+    raw: String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -20,17 +21,17 @@ struct ProcessOutput {
 #[derive(Serialize, Deserialize)]
 struct ErrorResponse {
     error: String,
-    details: Option<String>
+    details: Option<String>,
 }
 
 #[get("/health")]
-fn health() ->Json<serde_json::Value> {
+fn health() -> Json<serde_json::Value> {
     return Json(serde_json::json!({
         "status": "ok"
     }));
 }
 
-#[post("/run", format="json", data="<code>")]
+#[post("/run", format = "json", data = "<code>")]
 fn run_code(code: Json<Code>) -> Json<ProcessOutput> {
     let code = code.into_inner();
     let output = Command::new("qalam")
@@ -41,23 +42,27 @@ fn run_code(code: Json<Code>) -> Json<ProcessOutput> {
     let stdout = String::from_utf8_lossy(&output.stdout);
     let stderr = String::from_utf8_lossy(&output.stderr);
 
-    return Json(ProcessOutput { stdout: Some(stdout.to_string()), stderr: Some(stderr.to_string()) });
+    return Json(ProcessOutput {
+        stdout: Some(stdout.to_string()),
+        stderr: Some(stderr.to_string()),
+    });
 }
 
 #[catch(404)]
 fn not_found(req: &Request) -> Json<ErrorResponse> {
-    return Json(ErrorResponse { 
-        error: "Resource not found".to_string(), 
-        details: Some(format!("No route found for {}", req.uri())) 
-    })
+    return Json(ErrorResponse {
+        error: "Resource not found".to_string(),
+        details: Some(format!("No route found for {}", req.uri())),
+    });
 }
 
 #[catch(422)]
 fn unprocessable(_: &Request) -> Json<ErrorResponse> {
     return Json(ErrorResponse {
-        error: "The request was well-formed but was unable to be followed due to semantic errors".to_string(),
+        error: "The request was well-formed but was unable to be followed due to semantic errors"
+            .to_string(),
         details: None,
-    })
+    });
 }
 
 fn cors_midlleware() -> Cors {
@@ -71,7 +76,9 @@ fn cors_midlleware() -> Cors {
         allowed_headers: rocket_cors::AllowedHeaders::some(&["Authorization", "Content-Type"]),
         allow_credentials: true,
         ..Default::default()
-    }.to_cors().expect("Error generating CORS middleware")
+    }
+    .to_cors()
+    .expect("Error generating CORS middleware")
 }
 
 #[launch]
